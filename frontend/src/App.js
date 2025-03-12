92176f4696b46f7c51c4556497b12c591ba63ce6
Shreenaag <shreenaag2003@gmail.com>
2025-03-12 18:40:09 +0530

    Added First Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import LandingPage from './components/LandingPage';
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import FirstPage from './components/FirstPage';
+import MainPage from './components/MainPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+            <Route  path="/" element={<FirstPage/>} />
+            <Route path="/main" element={<MainPage />} />
+    </Routes>
+    </BrowserRouter>
+);
 }
 
 export default App;
