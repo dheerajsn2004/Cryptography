cb40ca76087acf2b3f76db7808da15d1530b37e1
dheerajsn2004 <sndheeraj2004@gmail.com>
2025-03-13 16:30:48 +0530

    saved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter , Routes, Route } from "react-router-dom";
-import FirstPage from './components/FirstPage';
-import MainPage from './components/MainPage';
+import FirstPage from './components/FirstPage.js';
+import MainPage from './components/MainPage.js';
+import EmailPage from './components/EmailPage.js';
 import './App.css';
-
+import SentPage from './components/SentPage.js';
+import DraftsPage from './components/DraftsPage.js';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
             <Route  path="/" element={<FirstPage/>} />
             <Route path="/main" element={<MainPage />} />
+            <Route path="/email" element={<EmailPage />} />
+            <Route path="/sent" element={<SentPage />} />
+            <Route path="/drafts" element={<DraftsPage />} />
+
     </Routes>
     </BrowserRouter>
 );
